817ff5bf638d243dfc63671b33869ff652311f34
Alinur Kaan <Alinurkaanbaytan@hotmail.com>
2019-11-21 01:34:15 +0100

    bak bu güncellemeleri yaptım

---
+++ Kaan/Main.java
@@ -0,0 +1,19 @@
+package Kaan;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int bankaparasý;
+		
+		System.out.println("Aylýk Harcama Planýna Hoþgeldiniz");
+		System.out.println("Baþlamak için bir tanesini þeiniz");
+		System.out.println("1.(Aylýk Kazancým) , 2.(Elektrik ve Su Parasý) , 3.(Market Alýþveriþi) , 4.(Kaç Tane Çocuk Var?)");
+		
+	}
+
+}
