1a198345990ebd82b79b452b7a3441d58b7f5c74
Alinur Kaan <Alinurkaanbaytan@hotmail.com>
2019-11-23 14:38:59 +0100

    Kaan

--- Kaan/Main.java
+++ Kaan/Main.java
@@ -8,21 +8,7 @@ public class Main {
 		
 		Scanner scanner = new Scanner(System.in);
 		
-		int bankaparasý;
-		
-		System.out.println("Aylýk Harcama Planýna Hoþgeldiniz");
-		System.out.println("Baþlamak için bir tanesini þeiniz");
-		System.out.println("1.(Aylýk Kazancým) , 2.(Elektrik ve Su Parasý) , 3.(Market Alýþveriþi) , 4.(Kaç Tane Çocuk Var?)");
-		
-		
-		System.out.println("1.(Aylýk Kazancým) , 2.(Elektrik ve Su Parasý) , 3.(Market Alýþveriþi) , 4.(Kaç Tane Çocuk Var?)");
-		System.out.println("1.(Aylýk Kazancým) , 2.(Elektrik ve Su Parasý) , 3.(Market Alýþveriþi) , 4.(Kaç Tane Çocuk Var?)");
-		System.out.println("1.(Aylýk Kazancým) , 2.(Elektrik ve Su Parasý) , 3.(Market Alýþveriþi) , 4.(Kaç Tane Çocuk Var?)");
-		
-		System.out.println("1.(Aylýk Kazancým) , 2.(Elektrik ve Su Parasý) , 3.(Market Alýþveriþi) , 4.(Kaç Tane Çocuk Var?)");
-		System.out.println("1.(Aylýk Kazancým) , 2.(Elektrik ve Su Parasý) , 3.(Market Alýþveriþi) , 4.(Kaç Tane Çocuk Var?)");
-		System.out.println("1.(Aylýk Kazancým) , 2.(Elektrik ve Su Parasý) , 3.(Market Alýþveriþi) , 4.(Kaç Tane Çocuk Var?)");
-		
+
 	}
 
 }
